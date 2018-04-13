fb08544ca13c978d4e0a967a6447b755444e614a
Mylon <mylonsuren@yahoo.ca>
2018-04-13 14:18:15 -0400

    Basic response framework setup

--- app.js
+++ app.js
@@ -1,8 +1,39 @@
 
 // discord-bot-test
 
-var express = require('express');
-var discord = require('discord.js');
+var Discord = require('discord.js');
 var auth = require('./auth.json');
+
+
+const client = new Discord.Client({
+  token: auth.token,
+  autorun: true
+});
+
+
+client.on('ready', () => {
+  console.log('Client is ready.');
+});
+
+
+client.on('message', message => {
+
+  if (message.content.substring(0,1) == '!') {
+    var args = message.content.substring(1).split(' ');
+    var cmd = args[0];
+    console.log("CMD : " + cmd);
+
+    switch(cmd) {
+      case 'ping' :
+        message.channel.send('Pong!');
+        break;
+      default : 
+        message.channel.send('Invalid message');
+    }
+  }
+});
+
+
+client.login(auth.token);
 
 
