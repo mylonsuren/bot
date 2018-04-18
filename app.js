2c72ce40facc75fcb7386aecc68f2dfeac705a10
Mylon <mylonsuren@yahoo.ca>
2018-04-18 16:10:24 -0400

    update

--- app.js
+++ app.js
@@ -11,14 +11,14 @@ const members = require("./data/members.json");
 const other = require("./data/other.json");
 const app = express();
 
-// app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 5000));
 
-// app.get('/', function (request, response) {
-//   var result = 'App is running';
-//   response.send(result);
-// }).listen(app.get('port'), function () {
-//   console.log('App is running, server is listening on port ', app.get('port'));
-// });
+app.get('/', function (request, response) {
+  var result = 'App is running';
+  response.send(result);
+}).listen(app.get('port'), function () {
+  console.log('App is running, server is listening on port ', app.get('port'));
+});
 
 const client = new discord.Client({
   token: auth.token,
