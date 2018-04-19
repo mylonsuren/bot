9024845fb53b75d99929df9362320415e4343edb
Mylon <mylonsuren@yahoo.ca>
2018-04-18 22:25:26 -0400

    update

--- app.js
+++ app.js
@@ -19,140 +19,144 @@ app.get('/', function (request, response) {
 }).listen(app.get('port'), function () {
   console.log('App is running, server is listening on port ', app.get('port'));
 
+  var http = require("http");
+  setInterval(function () {
+    http.get("https://turtles-bot.herokuapp.com/");
+  }, 300000);
 
-const client = new discord.Client({
-  token: auth.token,
-  autorun: true
-});
-
-
-client.on('ready', () => {
-  console.log('Client is ready.');
-  client.user.setActivity('Fullmetal Alchemist', {type: 'WATCHING'})
-    .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : 'none'}`))
-    .catch(console.error);
-});
-
-client.on('message', message => {
-
-  if (message.content.substring(0, 1) == '!') {
-    var args = message.content.substring(1).split(' ');
-    var cmd = args[0];
-    switch (cmd) {
-      case 'kick':
-        kickUser(message);
-        break;
-      case 'anime':
-        searchAnime(message);
-        break;
-      case 'name':
-        changeName(message);
-        break;
-      case 'add' :
-        inviteUser(message);
-        break;
-      case '?' :
-      case 'help' :
-        help(message);
-    }
-  } else if (message.content.toLowerCase().includes(other.n)) {
-    message.channel.send(other.reply1);
-  }
-});
+  const client = new discord.Client({
+    token: auth.token,
+    autorun: true
+  });
 
 
-client.login(auth.token);
+  client.on('ready', () => {
+    console.log('Client is ready.');
+    client.user.setActivity('Fullmetal Alchemist', { type: 'WATCHING' })
+      .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : 'none'}`))
+      .catch(console.error);
+  });
 
-function help (message) {
-  const text = "**Bot Commands** \n Kicking Members: !kick @Member \n Change Name: !name @Member newName \n Find Anime : !anime animeTitle";
-  message.channel.send(text);
-}
+  client.on('message', message => {
 
-function inviteUser(message) {
-  const user = message.content.split(" ")[1];
-  // console.log(user);
-
-  // console.log(message.guild.members);
-
-  message.channel.createInvite()
-    .then(invite => getUser(message, user, invite))
-    .catch(console.error)
-}
-
-function getUser(message, user, invite) {
-  for (key in members) {
-    var member = members[key];
-    // console.log(member);
-    // console.log(user);
-    if (member.names.includes(user)) {
-      // console.log(client.fetchUser(member.id));
-      // console.log(invite.url)
-      client.fetchUser(member.id)
-        .then(person => dmUser(person, invite))
-        .catch(console.error);
-
-      break;
-    }
-  }
-}
-
-function dmUser(user, invite) {
-  // user.createDM();
-  console.log(invite.url);
-  console.log(user);
-  user.send(invite.url);
-}
-
-function changeName(message) {
-  const users = message.mentions.users;
-
-  users.forEach(function (user) {
-    const id = user.id;
-    const member = message.guild.members.get(id);
-    const newName = message.content.split(" ");
-    var name = newName[2];
-    newName.forEach(function (word, index) {
-      if (index >= 3) {
-        name += " " + word;
+    if (message.content.substring(0, 1) == '!') {
+      var args = message.content.substring(1).split(' ');
+      var cmd = args[0];
+      switch (cmd) {
+        case 'kick':
+          kickUser(message);
+          break;
+        case 'anime':
+          searchAnime(message);
+          break;
+        case 'name':
+          changeName(message);
+          break;
+        case 'add':
+          inviteUser(message);
+          break;
+        case '?':
+        case 'help':
+          help(message);
       }
-    });
-    member.setNickname(name);
-  });
-}
-
-function kickUser(message) {
-  const users = message.mentions.users;
-  users.forEach(function (user) {
-    console.log(user);
-    const id = user.id;
-    const member = message.guild.members.get(id);
-    if (user.username.toLowerCase() === members[0].names[0].toLowerCase()) {
-      return;
-    } else if (message.author.username.toLowerCase === members[3].names[0].toLowerCase()) {
-      return;
+    } else if (message.content.toLowerCase().includes(other.n)) {
+      message.channel.send(other.reply1);
     }
-    member.kick();
-    console.log(message.author.username);
-    message.channel.send(user.username + " was removed by " + message.author);
   });
-}
-
-function searchAnime(message) {
-  var animeSearch = message.content.split("!anime ")[1];
-  console.log(animeSearch);
-  getRequest(animeSearch, message);
-}
-
-function composeMessageSuccess(list, message) {
-  var response = "**" + list[0].title.romaji + "**";
-
-  message.channel.send(response + "\n " + other.url2 + list[0].id)
-
-}
 
 
-function getRequest(value, message) {
-  var query = `
+  client.login(auth.token);
+
+  function help(message) {
+    const text = "**Bot Commands** \n Kicking Members: !kick @Member \n Change Name: !name @Member newName \n Find Anime : !anime animeTitle";
+    message.channel.send(text);
+  }
+
+  function inviteUser(message) {
+    const user = message.content.split(" ")[1];
+    // console.log(user);
+
+    // console.log(message.guild.members);
+
+    message.channel.createInvite()
+      .then(invite => getUser(message, user, invite))
+      .catch(console.error)
+  }
+
+  function getUser(message, user, invite) {
+    for (key in members) {
+      var member = members[key];
+      // console.log(member);
+      // console.log(user);
+      if (member.names.includes(user)) {
+        // console.log(client.fetchUser(member.id));
+        // console.log(invite.url)
+        client.fetchUser(member.id)
+          .then(person => dmUser(person, invite))
+          .catch(console.error);
+
+        break;
+      }
+    }
+  }
+
+  function dmUser(user, invite) {
+    // user.createDM();
+    console.log(invite.url);
+    console.log(user);
+    user.send(invite.url);
+  }
+
+  function changeName(message) {
+    const users = message.mentions.users;
+
+    users.forEach(function (user) {
+      const id = user.id;
+      const member = message.guild.members.get(id);
+      const newName = message.content.split(" ");
+      var name = newName[2];
+      newName.forEach(function (word, index) {
+        if (index >= 3) {
+          name += " " + word;
+        }
+      });
+      member.setNickname(name);
+    });
+  }
+
+  function kickUser(message) {
+    const users = message.mentions.users;
+    users.forEach(function (user) {
+      console.log(user);
+      const id = user.id;
+      const member = message.guild.members.get(id);
+      if (user.username.toLowerCase() === members[0].names[0].toLowerCase()) {
+        return;
+      } else if (message.author.username.toLowerCase === members[3].names[0].toLowerCase()) {
+        return;
+      }
+      member.kick();
+      console.log(message.author.username);
+      message.channel.send(user.username + " was removed by " + message.author);
+    });
+  }
+
+  function searchAnime(message) {
+    var animeSearch = message.content.split("!anime ")[1];
+    console.log(animeSearch);
+    getRequest(animeSearch, message);
+  }
+
+  function composeMessageSuccess(list, message) {
+    var response = "**" + list[0].title.romaji + "**";
+
+    message.channel.send(response + "\n " + other.url2 + list[0].id)
+
+  }
+
+
+  function getRequest(value, message) {
+    var query = `
     query ($id: Int, $page: Int, $perPage: Int, $search: String) {
       Page (page: $page, perPage: $perPage) {
         pageInfo {
@@ -171,48 +175,48 @@ function getRequest(value, message) {
       }
     }`;
 
-  var variables = {
-    search: value,
-    page: 1,
-    perPage: 1
-  };
-
-  var url = other.url,
-    options = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-      },
-      body: JSON.stringify({
-        query: query,
-        variables: variables
-      })
+    var variables = {
+      search: value,
+      page: 1,
+      perPage: 1
     };
 
-  fetch(url, options).then(handleResponse)
-    .then(handleData)
-    .catch(handleError);
+    var url = other.url,
+      options = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+          query: query,
+          variables: variables
+        })
+      };
 
-  function handleResponse(response) {
-    return response.json().then(function (json) {
-      return response.ok ? json : Promise.reject(json);
-    });
+    fetch(url, options).then(handleResponse)
+      .then(handleData)
+      .catch(handleError);
+
+    function handleResponse(response) {
+      return response.json().then(function (json) {
+        return response.ok ? json : Promise.reject(json);
+      });
+    }
+
+    function handleData(data) {
+      var list = data.data;
+      var text = list.Page.media;
+      text.forEach(function (anime) {
+        console.log(anime.id);
+      });
+
+      composeMessageSuccess(text, message);
+    }
+
+    function handleError(error) {
+      console.error(error);
+      message.channel.send(other.reply2);
+    }
   }
-
-  function handleData(data) {
-    var list = data.data;
-    var text = list.Page.media;
-    text.forEach(function (anime) {
-      console.log(anime.id);
-    });
-
-    composeMessageSuccess(text, message);
-  }
-
-  function handleError(error) {
-    console.error(error);
-    message.channel.send(other.reply2);
-  }
-}
 });
