d40852b9a272711e0e7072881f002fe7e9121fd1
Mylon <mylonsuren@yahoo.ca>
2018-04-20 17:30:58 -0400

    update

--- scripts/googleSearch.js
+++ scripts/googleSearch.js
@@ -18,20 +18,24 @@ const app = express();
 var googleCommand = function (msg) {
 
   const searchRequest = msg.content.split("!google ")[1];
-  console.log(searchRequest);
-
-  let searchUrl = `https://www.google.com/search?q=${encodeURIComponent(searchRequest)}`;
+  // console.log(searchRequest);
+  
+  var searchUrl = `https://www.google.com/search?q=${encodeURIComponent(searchRequest)}`;
 
   return snekfetch.get(searchUrl).then((result) => {
     let $ = cheerio.load(result.text);
     let googleData = $('.r').first().find('a').first().attr('href');
     googleData = querystring.parse(googleData.replace('/url?', ''));
-    console.log(googleData);
-
-    msg.channel.send(googleData.q);
+    // console.log(googleData);
+    var searchMap = searchRequest.replace(" ", "+");
     if (googleData['https://maps.google.com/maps?um']) {
-      var searchMap = searchRequest.replace(" ", "+");
+      msg.channel.send('***' + googleData.q + "***");
       msg.channel.send('https://www.google.com/maps/search/?api=1&query=' + searchMap);
+    } else if (googleData.q) {
+      msg.channel.send(googleData.q);
+    } else {
+      // autocorrectSearch(googleData['/search?q'], msg.channel);
+      msg.channel.send("*Try searching for: " + googleData['/search?q'] + "*");
     }
 
 
@@ -40,5 +44,32 @@ var googleCommand = function (msg) {
     console.log(err);
   });
 }
+
+// var autocorrectSearch = function(newMessage, channel) {
+//   const searchRequest = newMessage;
+//   console.log(searchRequest);
+  
+//   var searchUrl = `https://www.google.com/search?q=${encodeURIComponent(searchRequest)}`;
+
+//   return snekfetch.get(searchUrl).then((result) => {
+//     let $ = cheerio.load(result.text);
+//     let googleData = $('.r').first().find('a').first().attr('href');
+//     googleData = querystring.parse(googleData.replace('/url?', ''));
+//     console.log(googleData);
+//     var searchMap = searchRequest.replace(" ", "+");
+//     if (googleData['https://maps.google.com/maps?um']) {
+//       channel.send('***' + googleData.q + "***");
+//       channel.send('https://www.google.com/maps/search/?api=1&query=' + searchMap);
+//     } else if (googleData.q) {
+//       channel.send(googleData.q);
+//     } else {
+//       channel.send("No results found for your search: *" + searchRequest + "*");
+//     }
+
+//   }).catch((err) => {
+//     channel.send("No results found for your search: *" + searchRequest + "*");
+//     console.log(err);
+//   });
+// }
 
 module.exports = googleCommand;
