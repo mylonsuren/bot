6bd91169fff166a1ab2922eb6ea65a1b95a57c05
unknown <mylonsuren@gmail.com>
2021-11-20 17:49:29 -0500

    Secret Santa - update 1

---
+++ scripts/santa.js
@@ -0,0 +1,12 @@
+
+const discord = require('discord.js');
+const request = require('request');
+const fetch = require('isomorphic-fetch')
+const express = require('express');
+const http = require("http");
+const auth = require("../data/auth.json");
+const members = require("../data/members.json");
+const other = require("../data/other.json");
+const app = express();
+
+
