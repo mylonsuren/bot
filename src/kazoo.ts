761bdacc0cd1511599737b3c4ccc9db2a195e845
Mylon <mylonsuren@gmail.com>
2021-11-30 19:03:47 -0500

    Moved key words from kazoo to external data file

--- src/kazoo.ts
+++ src/kazoo.ts
@@ -1,15 +1,14 @@
 import { Message } from "discord.js";
+import { PRAT_KAZOO } from "../../data/censor.json";
 import { VIDEOS } from "../../data/media.json";
-import members from "../../data/members.json";
 import { sendReplyToMessage } from "./sendMessage";
 
 export const replyPratKazooVideo = async (message: Message) => {
 	const content = message.content;
 
 	if (
-		content.toLowerCase().includes("prat") &&
-		content.toLowerCase().includes("dumb") &&
-		message.author.id === members.Mylon.id
+		content.toLowerCase().includes(PRAT_KAZOO.key1) &&
+		content.toLowerCase().includes(PRAT_KAZOO.key2)
 	) {
 		await sendReplyToMessage(
 			message,
